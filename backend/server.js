c1716203f0b95627c2217fd4746b15da0a6e0bcb
Manav Kashyap <manavkashyap6708@gmail.com>
2025-08-28 21:14:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,3 +51,8 @@ if (fs.existsSync(clientBuild)) {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+// ...
+app.use("/api/webhooks", require("./routes/webhookRoutes")); // <--- add this
+// ...
+
